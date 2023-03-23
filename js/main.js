2434e25ba4742f1be171bc2dd7de04b79736eae7
Howgs <howgs111@gmail.com>
2023-03-22 23:52:07 -0300

    refactor: melhorando manipulaDados com forEach

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 const somar = document.querySelector("#somar");
 const subtrair = document.querySelector("#subtrair");
 const braco = document.querySelector("#braco");
+const controle = document.querySelectorAll(".controle-ajuste")
 
 
-somar.addEventListener("click", () => { 
-    manipulaDados("somar");
-})
-
-subtrair.addEventListener("click", () => { 
-    manipulaDados("subtrair")
-})
+controle.forEach ( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+    }
+)
 
 function manipulaDados(operacao) {
-    if(operacao === "somar") {
+    if(operacao === "+") {
         braco.value = parseInt(braco.value) + 1;
     }
 
